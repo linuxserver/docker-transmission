6bad2d716e976a5dde8d5ddcd5d573837e7fb336
Paul Hendryx <phendryx@gmail.com>
2016-11-02 10:04:54 -0400

    replace &amp; with &, static filename output for wget

--- root/defaults/blocklist-update.sh
+++ root/defaults/blocklist-update.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/with-contenv bash
 
 BLOCKLIST_ENABLED=`jq -r '.["blocklist-enabled"]' settings.json`
-BLOCKLIST_URL=`jq -r '.["blocklist-url"]' settings.json`
+BLOCKLIST_URL=`jq -r '.["blocklist-url"]' settings.json | sed 's/\&amp;/\&/g'`
 
 if [ $BLOCKLIST_ENABLED == true ]; then
 	mkdir -p /tmp/blocklists
 	rm -rf /tmp/blocklists/*
 	cd /tmp/blocklists
-	wget -q $BLOCKLIST_URL
+	wget -q -O blocklist.gz "$BLOCKLIST_URL"
 	if [ $? == 0 ]; then
 		gunzip *.gz
 		if [ $? == 0 ]; then
