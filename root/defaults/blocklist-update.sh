72c8df500ed9bb5ff92322a86327041d865b9ca9
Paul Hendryx <phendryx@gmail.com>
2016-11-02 09:39:55 -0400

    working script from container

--- root/defaults/blocklist-update.sh
+++ root/defaults/blocklist-update.sh
@@ -3,15 +3,16 @@
 BLOCKLIST_ENABLED=`jq -r '.["blocklist-enabled"]' settings.json`
 BLOCKLIST_URL=`jq -r '.["blocklist-url"]' settings.json`
 
-if [ $BLOCKLIST_URL == true ]; then
-	mkdir -p /tmp/blocklist
-	cd /tmp/blocklist
+if [ $BLOCKLIST_ENABLED == true ]; then
+	mkdir -p /tmp/blocklists
+	rm -rf /tmp/blocklists/*
+	cd /tmp/blocklists
 	wget -q $BLOCKLIST_URL
 	if [ $? == 0 ]; then
 		gunzip *.gz
 		if [ $? == 0 ]; then
 			chmod go+r *
-			rm /config/blocklists/*
+			rm -rf /config/blocklists/*
 			cp /tmp/blocklists/* /config/blocklists
 			s6-svc -h /var/run/s6/services/transmission
 		fi
