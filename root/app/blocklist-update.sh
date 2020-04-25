07a6186ccfb967eb0608032d08e7c727c8595366
Sam Quinn <Quinnsam1@gmail.com>
2020-04-25 12:24:52 -0700

    Updated blocklist-update.sh to allow for custom scripts located in the /config folder

--- root/app/blocklist-update.sh
+++ root/app/blocklist-update.sh
@@ -2,19 +2,39 @@
 
 BLOCKLIST_ENABLED=`jq -r '.["blocklist-enabled"]' /config/settings.json`
 BLOCKLIST_URL=`jq -r '.["blocklist-url"]' /config/settings.json | sed 's/\&amp;/\&/g'`
+SCRIPT_DEBUG=false
 
-if [ $BLOCKLIST_ENABLED == true ]; then
+# Exit if Block list is not enabled
+[ $BLOCKLIST_ENABLED == true ] || exit 0
+
+# Check if custom script exists in config folder
+if [ -e /config/blocklist-update.sh ]; then
+	[ $SCRIPT_DEBUG == true ] && echo "Custom script found Running [/config/update-blocklist.sh]"
+	/config/blocklist-update.sh
+	ret=$((ret + $?))
+	[ $SCRIPT_DEBUG == true ] && echo "Custom script at [/config/blocklist-update.sh] exited with [$ret]"
+else
+	[ $SCRIPT_DEBUG == true ] && echo "Running [/app/update-blocklist.sh]"
 	mkdir -p /tmp/blocklists
 	rm -rf /tmp/blocklists/*
 	cd /tmp/blocklists
 	wget -q -O blocklist.gz "$BLOCKLIST_URL"
-	if [ $? == 0 ]; then
+	ret=$((ret + $?))
+	[ $SCRIPT_DEBUG == true ] && echo "blocklist at [$BLOCKLIST_URL] downloaded exited with [$ret]"
+	if [ $ret == 0 ]; then
 		gunzip *.gz
-		if [ $? == 0 ]; then
+		ret=$((ret + $?))
+		[ $SCRIPT_DEBUG == true ] && echo "Gunzip extracted [$(ls)] and exited with [$ret]"
+		if [ $ret == 0 ]; then
 			chmod go+r *
 			rm -rf /config/blocklists/*
 			cp /tmp/blocklists/* /config/blocklists
-			s6-svc -t /var/run/s6/services/transmission
+			[ $SCRIPT_DEBUG == true ] && echo "[$(ls /tmp/blocklists/*)] copied to [/config/blocklists]"
 		fi
 	fi
 fi
+
+
+[ $SCRIPT_DEBUG == true ] && echo "Transmission will restart if return values [$ret] is equal to \"0\""
+# Restart Transmission after blocklist update
+[ $ret == 0 ] && s6-svc -t /var/run/s6/services/transmission
