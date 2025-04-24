52fc6098ff1232061aed2df04bfdbb743ff83742
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-04-24 19:30:13 +0300

    account for changed port

--- root/app/blocklist-update.sh
+++ root/app/blocklist-update.sh
@@ -2,11 +2,12 @@
 # shellcheck shell=bash
 
 BLOCKLIST_ENABLED=$(jq -r '.["blocklist-enabled"]' /config/settings.json)
+PORT=$(jq '.["rpc-port"]' /config/settings.json)
 
 if [[ "$BLOCKLIST_ENABLED" == true ]]; then
     if [[ -n "$USER" ]] && [[ -n "$PASS" ]]; then
-        /usr/bin/transmission-remote -n "$USER":"$PASS" --blocklist-update
+        /usr/bin/transmission-remote 127.0.0.1:${PORT:-9091} -n "$USER":"$PASS" --blocklist-update
     else
-        /usr/bin/transmission-remote --blocklist-update
+        /usr/bin/transmission-remote 127.0.0.1:${PORT:-9091} --blocklist-update
     fi
 fi
